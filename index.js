1b49ca7a9f3b930528782ff5250e167324e86eec
fgyhuji8 <js1loner665@hotmail.com>
2024-05-11 05:29:46 +0700

    start project commit

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-function combinationSum3(k, n) {
-  const result = [];
-  backtrack([], 1, k, n);
-  return result;
-  function backtrack(combination, start, k, n) {
-    if (n === 0 && k === 0) {
-      result.push([...combination]);
-      return;
-    }
-    if (n < 0 || k === 0) return;
-    for (let i = start; i <= 9; i++) {
-      combination.push(i);
-      backtrack(combination, i + 1, k - 1, n - i);
-      combination.pop();
-    }
-  }
+function findKthLargest(nums, k) {
+  nums.sort((a, b) => b - a);
+  return nums[k - 1];
 }
